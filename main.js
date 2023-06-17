da95a0df655c71350dd6e6f4c273d578e11591ab
wjkk21 <verstand821@gmail.com>
2023-06-17 18:48:45 +0900

    뭐해야되지

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const root = document.getElementById('root')
+const MyName = document.querySelector('MyName')
 
-const tagMaker = function (tagName,innerText,appen) {
+const tagMaker = function (tagName,innerText,parent,className) {
   let a = document.createElement(tagName)
-    a.innerText = innerText
-  return appen.appendChild(a)
+  a.innerText = innerText
+  a.setAttribute('class', className);
+  return parent.appendChild(a)
 }
+tagMaker('h1', '- 임지성 -', root, 'MyName')
+tagMaker('div', '안녕하세요', root)
+tagMaker('div', ' js ts react git 블라블라', root)
+tagMaker('div', ' 이메일 전화번호 ', root)
+tagMaker('div', ' footer ', root)
+tagMaker('div', ' ;', root)
 
-console.log(tagMaker('div', 'ㅎㅇ', root))
-  console.log(root)
