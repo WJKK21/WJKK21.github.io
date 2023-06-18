0886e3ac01f126487b660f979509059859c3a4b2
wjkk21 <verstand821@gmail.com>
2023-06-18 22:38:51 +0900

    ????

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const root = document.getElementById('root')
-const MyName = document.querySelector('MyName')
 
-const tagMaker = function (tagName,innerText,parent,className) {
+const tagMaker = function (tagName, innerText, parent, className) {
   let a = document.createElement(tagName)
   a.innerText = innerText
-  a.setAttribute('class', className);
+  a.setAttribute('class', className)
   return parent.appendChild(a)
 }
 tagMaker('h1', '- 임지성 -', root, 'MyName')
+
 tagMaker('div', '안녕하세요', root)
 tagMaker('div', ' js ts react git 블라블라', root)
-tagMaker('div', ' 이메일 전화번호 ', root)
-tagMaker('div', ' footer ', root)
-tagMaker('div', ' ;', root)
+tagMaker('div', ' verstad821@gmail.com ', root)
+tagMaker('div', '  ', root)
 
+const MyName = document.querySelector('.MyName')
+MyName.style.display = 'flex'
+MyName.style.justifyContent = 'center'
