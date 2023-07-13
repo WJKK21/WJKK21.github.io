ad1b46f0f53b21ea3eaf195ae11bcf092a3b03a6
wjkk21 <verstand821@gmail.com>
2023-07-13 21:24:43 +0900

    css 삭제

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-const root = document.getElementById('root')
+const root = document.getElementById("root");
+
+document.querySelector("body").style.margin = "0";
+root.style.width = "100vw";
+root.style.height = "100vh";
+root.style.backgroundColor = "darkgray";
+root.style.display = "flex";
+root.style.flexDirection = "column";
+root.style.alignItems = "center";
 
 const tagMaker = function (tagName, innerText, parent, className) {
-  let a = document.createElement(tagName)
-  a.innerText = innerText
-  a.setAttribute('class', className)
-  return parent.appendChild(a)
-}
-tagMaker('h1', '- 임지성 -', root, 'MyName')
+  let a = document.createElement(tagName);
+  a.innerText = innerText;
+  a.setAttribute("class", className);
+  return parent.appendChild(a);
+};
 
-tagMaker('div', '안녕하세요', root)
-tagMaker('div', ' js ts react git 블라블라', root)
-tagMaker('div', ' verstad821@gmail.com ', root)
-tagMaker('div', '  ', root)
+tagMaker("div", "안녕하세요", root);
+tagMaker("div", "웹 개발을 하고 있는 임지성입니다", root);
+tagMaker("div", "  ", root);
+tagMaker(
+  "img scr = https://images.velog.io/images/rosarang/post/e42e7753-f6f8-439d-acf4-4911bcf1eac6/html.png",
+  "",
+  root
+);
+tagMaker("div", " verstad821@gmail.com ", root);
 
-const MyName = document.querySelector('.MyName')
-MyName.style.display = 'flex'
-MyName.style.justifyContent = 'center'
+const MyName = document.querySelector(".MyName");
+MyName.style.display = "flex";
+MyName.style.justifyContent = "center";
