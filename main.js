90ce8bf1284a66d7240ff3c185b5528d882c4d95
wjkk21 <verstand821@gmail.com>
2023-07-13 21:42:45 +0900

    이미지 넣는거 찾아봄

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+document.querySelector("body").style.margin = "0";
+
 const root = document.getElementById("root");
 
-document.querySelector("body").style.margin = "0";
 root.style.width = "100vw";
 root.style.height = "100vh";
 root.style.backgroundColor = "darkgray";
@@ -8,22 +9,30 @@ root.style.display = "flex";
 root.style.flexDirection = "column";
 root.style.alignItems = "center";
 
-const tagMaker = function (tagName, innerText, parent, className) {
-  let a = document.createElement(tagName);
-  a.innerText = innerText;
-  a.setAttribute("class", className);
-  return parent.appendChild(a);
+const tagMaker = function (tagName, innerText, attributes) {
+  let tag = document.createElement(tagName);
+
+  if (innerText) {
+    tag.innerText = innerText;
+  }
+
+  for (var key in attributes) {
+    if (attributes.hasOwnProperty(key)) {
+      tag.setAttribute(key, attributes[key]);
+    }
+  }
+
+  return root.appendChild(tag);
 };
 
-tagMaker("div", "안녕하세요", root);
-tagMaker("div", "웹 개발을 하고 있는 임지성입니다", root);
-tagMaker("div", "  ", root);
-tagMaker(
-  "img scr = https://images.velog.io/images/rosarang/post/e42e7753-f6f8-439d-acf4-4911bcf1eac6/html.png",
-  "",
-  root
-);
-tagMaker("div", " verstad821@gmail.com ", root);
+tagMaker("div", "안녕하세요", { id: "haeder" });
+tagMaker("div", "임지성입니다");
+tagMaker("div", "");
+tagMaker("img", "123", {
+  src: "https://images.velog.io/images/goblin820/post/68db173f-a375-4209-b4e6-f08191f7916c/VelogThumbnail_1.png",
+  alt: "스크립트 꺼졌는데요?",
+});
+tagMaker("div", "verstand821@gmail.com ");
 
 const MyName = document.querySelector(".MyName");
 MyName.style.display = "flex";
